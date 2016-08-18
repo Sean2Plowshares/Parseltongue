4a8556fbfaf30134019e743b5339f3910454da6a
Sean Plowman <dropkickdude23@gmail.com>
2016-08-17 20:29:46 -0400

    added header

---
+++ ToDo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env pyhton3
+
+print("""
+Welcome to Super Todo! This is a CLI to organize your life like it's 1999!
+Just type a command to get started. If you need to know what commands are
+available, just type 'help' or '?'
+""")
