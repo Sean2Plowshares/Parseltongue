216ec24d00f068c99b8f92e8e1584ea3d2cafdbd
Sean Plowman <dropkickdude23@gmail.com>
2016-08-18 11:40:40 -0400

    created input and command structure

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 An app for managing a todo list.
 ----------
 Things for ToDo to do:
-1. Prompt for user input
-1a. help - display all commands with tooltips
-1b. new - create new todo with prompt for description
-1c. list - show all todo names and index numbers
-1d. show - show description for indicated todo
+1. (done) Prompt for user input
+2. (done) help - display all commands with tooltips
+3. new - create new todo with prompt for description
+4. list - show all todo names and index numbers
+5. show - show description for indicated todo
 
-2. save list
+6. save list
--- ToDo.py
+++ ToDo.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env pyhton3
 
+def help():
+    print("Available commands are new, show, list, and quit.")
+
 print("""
 Welcome to Super Todo! This is a CLI to organize your life like it's 1999!
 Just type a command to get started. If you need to know what commands are
 available, just type 'help' or '?'
 """)
+
+while True:
+    command = input("Command:")
+
+    if command == "help" or command == "?":
+        help()
+    elif command == "new":
+        print("Sorry, I can't do that yet.")
+    elif command == "show":
+        print("Sorry, I can't do that yet.")
+    elif command == "list":
+        print("Sorry, I can't do that yet.")
+    elif command == "quit" or command == "exit":
+        print("Thanks for using Super Todo!")
+        break
+    else:
+        print("""
+        That's not a valid command.
+        Use commands 'help' or '?' for a list of valid commands.
+        ''")
