72f8bff63988f0438a5871bbf0e208b372c2415a
Sean Plowman <dropkickdude23@gmail.com>
2016-08-18 14:09:47 -0400

    functionaality for new and list commands

--- ToDo.py
+++ ToDo.py
@@ -1,7 +1,34 @@
-#!/usr/bin/env pyhton3
+#!/usr/bin/env python3
 
 def help():
     print("Available commands are new, show, list, and quit.")
+
+def new(command):
+    parsed_args = command.split(' ')[1:]
+    title = ' '.join(parsed_args)
+    print("Creating a new todo:", title)
+    print("Write a short description for this todo. Enter an empty line when finished.")
+    body = get_longform_user_input()
+    return {'title': title, 'body': body, }
+
+def get_longform_user_input():
+    lines = []
+    line = input()
+
+    while line != '':
+        lines.append(line)
+        line = input()
+
+    return '\n'.join(lines)
+
+def list ():
+    print()
+    print("Your current todo list:")
+    print()
+    for index, todo in enumerate(todos):
+        print("\t", index +1, todo['title'])
+        print()
+
 
 print("""
 Welcome to Super Todo! This is a CLI to organize your life like it's 1999!
@@ -10,12 +37,12 @@ available, just type 'help' or '?'
 """)
 
 while True:
-    command = input("Command:")
+    command = input("Command: ")
 
     if command == "help" or command == "?":
         help()
-    elif command == "new":
-        print("Sorry, I can't do that yet.")
+    elif command.startswith("new"):
+        todos.append(new(command))
     elif command == "show":
         print("Sorry, I can't do that yet.")
     elif command == "list":
@@ -24,7 +51,4 @@ while True:
         print("Thanks for using Super Todo!")
         break
     else:
-        print("""
-        That's not a valid command.
-        Use commands 'help' or '?' for a list of valid commands.
-        ''")
+        print("That's not a valid command. Use 'help' or '?' for more info.")
